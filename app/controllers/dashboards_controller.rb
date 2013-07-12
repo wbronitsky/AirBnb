c3692c3f5296b385bf7ce9e964a4a1305b5d0a14
wbronitsky <wbronitsky@gmail.com>
2013-07-12 10:49:33 -0700

    Fix current reservation bug

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,7 +6,7 @@ class DashboardsController < ApplicationController
     @trips = PlaceRentalRequest.includes(:place)
                                 .where(user_id: current_user.id)
                                 .order("begin_date")
-    @current_trips = @trips.select {|trip| trip.status == "approved" && trip.begin_date > Date.today}
+    @current_trips = @trips.select {|trip| trip.status == "approved" && trip.end_date > Date.today}
     @pending_trips = @trips.select {|trip| trip.status == nil && trip.begin_date > Date.today}
     @past_trips = @trips.select {|trip| trip.end_date < Date.today}
   end
